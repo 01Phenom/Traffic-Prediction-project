a08951d35c16c5298f4850bd97d68747351f7119
neze-papreja <143400973+neze-papreja@users.noreply.github.com>
2024-04-10 00:14:55 +0530

    Update about.py

--- Tabs/about.py
+++ Tabs/about.py
@@ -9,10 +9,10 @@ def app():
     """This function creates the about page"""
     st.snow()
     st.title('Data Mining Project')
-    st.markdown('''### SLIIT CODERS''')
-    st.markdown('''Passionate Engineer and Rational Thinker. Data Scientist, Web Developer''')
+    
+    st.markdown(''' Data Scientist Project''')
     image = Image.open('./images/icon.jpg')
     st.image(image)
     
-    st.markdown('''### GitHub: [Group 24](https://github.com/IT21365232/Traffic-Prediction-project.git)''')
+    
     
